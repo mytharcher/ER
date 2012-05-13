70df05b45c4753e47e34ecefcec2977c066c5c8f
mytharcher <mytharcher@gmail.com>
2012-05-13 14:51:31 +0800

    Add lib bridge for esui.

---
+++ src/esui/~lib/jslib.js
@@ -0,0 +1,232 @@
+///import esui.lib;
+
+///import js.util.Class;
+
+esui.lib.inherits = js.util.Class.inherit;
+
+esui.lib.extend = js.util.Class.mix;
+
+esui.lib.clone = js.util.Class.clone;
+
+
+
+///import js.dom.Stage.loadScript;
+
+esui.lib.loadScript = function (url, onload) {
+	js.dom.Stage.loadScript(url, {onload: onload});
+};
+
+
+
+
+///import js.client.Browser;
+
+esui.lib.firefox = js.Browser.Firefox;
+
+esui.lib.ie = js.client.Browser.IE;
+
+
+
+///import js.dom.Stage.get;
+
+esui.lib.g = js.dom.Stage.get;
+
+
+
+///import js.text.Template;
+
+esui.lib.format = js.text.Template.format;
+
+
+
+///import js.text.Encoder.encodeHTML;
+
+esui.lib.encodeHTML = js.text.Encoder.encodeHTML;
+
+
+
+///import js.net.Ajax;
+
+esui.lib.ajax = function (url, options) {
+	options.url = url;
+	return js.net.Ajax.request(options);
+};
+
+
+
+///import js.dom.Attribute;
+
+esui.lib.setAttribute = js.dom.Attribute.set;
+
+
+
+///import js.dom.Operation;
+
+esui.lib.insertAfter = js.dom.Operation.after;
+
+esui.lib.insertBefore = js.dom.Operation.before;
+
+
+
+///import js.client.Features.~arrayIndexOf;
+
+esui.lib.inArray = function (array, item) {
+	return array.indexOf(item) > -1;
+};
+
+
+
+esui.lib.formatDate = function (source, pattern) {
+    if ('string' != typeof pattern) {
+        return source.toString();
+    }
+
+    function replacer(patternPart, result) {
+        pattern = pattern.replace(patternPart, result);
+    }
+    
+    var pad     = baidu.number.pad,
+        year    = source.getFullYear(),
+        month   = source.getMonth() + 1,
+        date2   = source.getDate(),
+        hours   = source.getHours(),
+        minutes = source.getMinutes(),
+        seconds = source.getSeconds();
+
+    replacer(/yyyy/g, pad(year, 4));
+    replacer(/yy/g, pad(parseInt(year.toString().slice(2), 10), 2));
+    replacer(/MM/g, pad(month, 2));
+    replacer(/M/g, month);
+    replacer(/dd/g, pad(date2, 2));
+    replacer(/d/g, date2);
+
+    replacer(/HH/g, pad(hours, 2));
+    replacer(/H/g, hours);
+    replacer(/hh/g, pad(hours % 12, 2));
+    replacer(/h/g, hours % 12);
+    replacer(/mm/g, pad(minutes, 2));
+    replacer(/m/g, minutes);
+    replacer(/ss/g, pad(seconds, 2));
+    replacer(/s/g, seconds);
+
+    return pattern;
+};
+
+
+
+///import js.dom.BoxModel;
+
+esui.lib.getPosition = function (elem) {
+    return js.dom.BoxModel.getPosition(elem, document.body);
+};
+
+
+
+esui.lib.getPageWidth = function () {
+    var doc = document,
+        body = doc.body,
+        html = doc.documentElement,
+        client = doc.compatMode == 'BackCompat' ? body : doc.documentElement;
+
+    return Math.max(html.scrollWidth, body.scrollWidth, client.clientWidth);
+};
+
+
+
+///import js.dom.ClassName;
+
+esui.lib.addClass = js.dom.ClassName.add;
+
+esui.lib.removeClass = js.dom.ClassName.remove;
+
+esui.lib.hasClass = js.dom.ClassName.has;
+
+
+
+///import js.dom.Event.add;
+
+esui.lib.on = js.dom.Event.add;
+
+esui.lib.un = js.dom.Event.remove;
+
+
+
+///import js.dom.Drag;
+
+esui.lib.draggable = js.dom.Drag.attach;
+
+
+
+///import js.dom.Stage.getDocumentElement;
+
+esui.lib.getPageViewHeight = function () {
+    return js.dom.Stage.getDocumentElement().clientHeight;
+};
+
+esui.lib.getPageViewWidth = function () {
+    return js.dom.Stage.getDocumentElement().clientWidth;
+};
+
+
+
+esui.lib.getPageScrollTop = function () {
+    var d = document;
+    return window.pageYOffset || d.documentElement.scrollTop || d.body.scrollTop;
+};
+
+esui.lib.getPageScrollLeft = function () {
+    var d = document;
+    return window.pageXOffset || d.documentElement.scrollLeft || d.body.scrollLeft;
+};
+
+
+
+///import js.dom.Relation;
+
+esui.lib.firstChild = js.dom.Relation.firstChild;
+
+esui.lib.nextSibling = js.dom.Relation.next;
+
+esui.lib.elementContains = js.dom.Relation.contains;
+
+
+
+///import js.dom.MouseTracker;
+
+esui.lib.getMousePosition = function () {
+    var Tracker = js.dom.MouseTracker;
+    return {
+        x : esui.lib.getPageScrollLeft() + Tracker.x,
+        y : esui.lib.getPageScrollTop() + Tracker.y
+    };
+};
+
+
+
+esui.lib.isWebkit = /webkit/i.test(navigator.userAgent);
+
+
+
+///import js.dom.Style;
+
+esui.lib.getStyle = js.dom.Style.get;
+
+
+
+esui.lib.preventDefaultEvent = function (ev) {
+    ev.preventDefault ? ev.preventDefault : (ev.returnValue = false);
+};
+
+
+
+///import js.client.Features.~stringTrim;
+
+esui.lib.trim = function (str) {
+    return str.trim();
+};
+
+
+
+///import js.dom.Selector;
+
+esui.lib.query = js.dom.Selector.queryAll;
---
+++ src/esui/~lib/tangram.js
@@ -0,0 +1,199 @@
+///import esui.lib;
+
+///import baidu.lang.inherits;
+
+esui.lib.inherits = baidu.lang.inherits;
+
+
+
+///import baidu.sio.callByBrowser;
+
+esui.lib.loadScript = baidu.sio.callByBrowser;
+
+
+
+///import baidu.object.extend;
+
+esui.lib.extend = baidu.object.extend;
+
+
+
+///import baidu.browser.firefox;
+
+esui.lib.firefox = baidu.browser.firefox;
+
+
+
+///import baidu.brower.ie;
+
+esui.lib.ie = baidu.browser.ie;
+
+
+
+///import baidu.dom.g;
+
+esui.lib.g = baidu.dom.g;
+
+
+
+///import baidu.string.format;
+
+esui.lib.format = baidu.string.format;
+
+
+
+///import baidu.object.clone;
+
+esui.lib.clone = baidu.object.clone;
+
+
+
+///import baidu.string.encodeHTML;
+
+esui.lib.encodeHTML = baidu.string.encodeHTML;
+
+
+
+///import baidu.ajax.request;
+
+esui.lib.ajax = baidu.ajax.request;
+
+
+
+///import baidu.dom.setAttr;
+
+esui.lib.setAttribute = baidu.dom.setAttr;
+
+
+
+///import baidu.dom.insertAfter;
+
+esui.lib.insertAfter = baidu.dom.insertAfter;
+
+
+
+///import baidu.dom.insertBefore;
+
+esui.lib.insertBefore = baidu.dom.insertBefore;
+
+
+
+///import baidu.array.contains;
+
+esui.lib.inArray = baidu.array.contains;
+
+
+
+///import baidu.date.format;
+
+esui.lib.formatDate = baidu.date.format;
+
+
+
+///import baidu.dom.getPosition;
+
+esui.lib.getPosition = baidu.dom.getPosition;
+
+
+
+///import baidu.page.getWidth;
+
+esui.lib.getPageWidth = baidu.page.getWidth;
+
+
+
+///import baidu.dom.addClass;
+
+esui.lib.addClass = baidu.dom.addClass;
+
+
+
+///import baidu.dom.removeClass;
+
+esui.lib.removeClass = baidu.dom.removeClass;
+
+
+
+///import baidu.dom.hasClass;
+
+esui.lib.hasClass = baidu.dom.hasClass;
+
+
+
+///import baidu.event.on;
+
+esui.lib.on = baidu.event.on;
+
+esui.lib.un = baidu.event.un;
+
+
+
+///import baidu.dom.draggable;
+
+esui.lib.draggable = baidu.dom.draggable;
+
+
+
+///import baidu.page.getViewHeight;
+
+esui.lib.getPageVewHeight = baidu.page.getViewHeight;
+
+
+
+///import baidu.page.getScrollTop;
+
+esui.getPageScrollTop = baidu.page.getScrollTop;
+
+
+
+///import baidu.dom.first;
+
+esui.lib.firstChild = baidu.dom.first;
+
+
+
+///import baidu.dom.next;
+
+esui.lib.nextSibling = baidu.dom.next;
+
+
+
+///import baidu.dom.contains;
+
+esui.lib.elementContains = baidu.dom.contains;
+
+
+
+///import baidu.page.getMousePosition;
+
+esui.lib.getMousePosition = baidu.page.getMousePosition;
+
+
+
+///import baidu.browser.isWebkit;
+
+esui.lib.isWebkit = baidu.browser.isWebkit;
+
+
+
+///import baidu.dom.getStyle;
+
+esui.lib.getStyle = baidu.dom.getStyle;
+
+
+
+///import baidu.event.preventDefault;
+
+esui.lib.preventDefaultEvent = baidu.event.preventDefault;
+
+
+
+///import baidu.string.trim;
+
+esui.lib.trim = baidu.string.trim;
+
+
+
+///import baidu.dom.query;
+
+esui.lib.query = baidu.dom.query;
