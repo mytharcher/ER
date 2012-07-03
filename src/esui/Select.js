d52fbd59249622c077d3ccac4f74559b42af5b8e
mytharcher <mytharcher@gmail.com>
2012-07-03 15:32:52 +0800

    Fix wrong HTML content in title when null value.

--- src/esui/Select.js
+++ src/esui/Select.js
@@ -447,11 +447,12 @@ esui.Select.prototype = {
      */
     _repaint: function () {
         var selected = this.datasource[ this.selectedIndex ],
-            word = this.staticText || ( selected ? selected.name : this.emptyLabel ),
+            word = this.staticText || ( selected ? selected.name : this.emptyText ),
+            wordHTML = this.staticText || ( selected ? selected.name : this.emptyLabel ),
             el = this._getCur();
             
         el.title = word;
-        el.innerHTML = '<nobr>' + word + '</nobr>';
+        el.innerHTML = '<nobr>' + wordHTML + '</nobr>';
         
         this._repaintLayer();
     },
