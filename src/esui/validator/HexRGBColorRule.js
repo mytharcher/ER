92364bdcd90afaabd17fdaf0d3fcbfe4506fd0ef
mytharcher <mytharcher@gmail.com>
2012-09-03 12:56:32 +0800

    Add validation rule.

---
+++ src/esui/validator/HexRGBColorRule.js
@@ -0,0 +1,42 @@
+/**
+ * 十六进制RGB颜色串验证规则
+ * 
+ * @class
+ * @param {Object} options 参数
+ */
+esui.validator.HexRGBColorRule = function( options ) {
+    options = options || {};
+    options.errorMessage && (this.errorMessage = options.errorMessage);
+};
+
+esui.validator.HexRGBColorRule.prototype = {
+    /**
+     * 错误提示信息
+     * 
+     * @public
+     */
+    errorMessage : "${title}不是正确的颜色格式",
+
+    /**
+     * 获取规则名称
+     * 
+     * @public
+     * @return {string}
+     */
+    getName: function () {
+        return 'hexRGBColor';
+    },
+    
+    /**
+     * 验证值是否合法
+     * 
+     * @public
+     * @return {string}
+     */
+    check: function ( value ) {
+        return (/^[\da-f]{6}$/i).test(value);
+    }
+};
+
+baidu.inherits( esui.validator.HexRGBColorRule, esui.validator.Rule );
+esui.validator.Rule.register( 'hexRGBColor', esui.validator.HexRGBColorRule );
